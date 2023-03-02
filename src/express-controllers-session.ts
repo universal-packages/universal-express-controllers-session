fcf343935f7e6975265e8be5102f5be3b9b75f4b
David De Anda <omarandstuff@gmail.com>
2023-03-01 23:17:50 -0600

    Upgrade packages and better initialization

--- src/express-controllers-session.ts
+++ src/express-controllers-session.ts
@@ -5,6 +5,8 @@ export const CURRENT_OPTIONS: ExpressSessionOptions = {}
 export function initialize(options?: ExpressSessionOptions): void {
   if (options && options.cookieName) CURRENT_OPTIONS.cookieName = options.cookieName
   if (options && options.engine) CURRENT_OPTIONS.engine = options.engine
+  if (options && options.engineOptions) CURRENT_OPTIONS.engineOptions = options.engineOptions
   if (options && options.registryId) CURRENT_OPTIONS.registryId = options.registryId
+  if (options && options.seed) CURRENT_OPTIONS.seed = options.seed
   if (options && options.trackSessionAccess) CURRENT_OPTIONS.trackSessionAccess = options.trackSessionAccess
 }
