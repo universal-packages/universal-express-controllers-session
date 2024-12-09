39b86ff038735101913198b0d37e8c532017b155
David De Anda <omarandstuff@gmail.com>
2024-12-08 19:48:01 -0800

    Dynamic options assignation

--- src/express-controllers-session.ts
+++ src/express-controllers-session.ts
@@ -3,10 +3,9 @@ import { ExpressSessionOptions } from '@universal-packages/express-session'
 export const CURRENT_OPTIONS: ExpressSessionOptions = {}
 
 export function initialize(options?: ExpressSessionOptions): void {
-  if (options && options.cookieName) CURRENT_OPTIONS.cookieName = options.cookieName
-  if (options && options.engine) CURRENT_OPTIONS.engine = options.engine
-  if (options && options.engineOptions) CURRENT_OPTIONS.engineOptions = options.engineOptions
-  if (options && options.registryId) CURRENT_OPTIONS.registryId = options.registryId
-  if (options && options.seed) CURRENT_OPTIONS.seed = options.seed
-  if (options && options.trackSessionAccess) CURRENT_OPTIONS.trackSessionAccess = options.trackSessionAccess
+  if (options) {
+    Object.keys(options).forEach((key) => {
+      if (options[key]) CURRENT_OPTIONS[key] = options[key]
+    })
+  }
 }
